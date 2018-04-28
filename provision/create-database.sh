80fac76283844a6d160357e1f4947ac53a307c90
Anton Syuvaev <syuvaev@ubic.tech>
2018-04-28 16:34:53 +0300

    database create script

---
+++ provision/create-database.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source /app/vagrant/provision/common.sh
+
+#== Import script args ==
+
+db=$(echo "$1")
+
+mysql -uroot <<< "CREATE DATABASE ${db} CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci"
+mysql -uroot <<< "CREATE USER '${db}'@'%' IDENTIFIED BY ''"
+mysql -uroot <<< "GRANT ALL PRIVILEGES ON ${db}.* TO '${db}'@'%'"
+mysql -uroot <<< "FLUSH PRIVILEGES"
