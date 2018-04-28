b6ff313b0f0ff329abe3e8936d814739c3465285
Anton Syuvaev <syuvaev@ubic.tech>
2018-04-28 18:52:41 +0300

    script for nginx config auto-generation

---
+++ provision/create-nginx-config.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+source /app/vagrant/provision/common.sh
+
+#== Import script args ==
+sitename=$(echo "$1")
+
+configs_path="/app/vagrant/nginx/sites"
+template_file="${configs_path}/project.conf.example"
+config_file="${configs_path}/${sitename}.conf"
+
+if [ ! -f $config_file ]
+then
+    info "Creating nginx config for $sitename"
+    cp "$template_file" "$config_file"
+    sed -i "s/PROJECT_NAME/$sitename/g" "$config_file"
+    echo "Done!"
+fi
