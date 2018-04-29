2aa730c1f025f959d3881265f65f0a1d608ad381
Anton Syuvaev <syuvaev@ubic.tech>
2018-04-29 17:50:31 +0300

    add check for existing DB

--- provision/create-database.sh
+++ provision/create-database.sh
@@ -5,9 +5,13 @@ source /app/vagrant/provision/common.sh
 #== Import script args ==
 
 db=$(echo "$1")
-info "Adding MySQL database: ${db}"
-mysql -uroot <<< "CREATE DATABASE ${db} CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci"
-mysql -uroot <<< "CREATE USER '${db}'@'%' IDENTIFIED BY ''"
-mysql -uroot <<< "GRANT ALL PRIVILEGES ON ${db}.* TO '${db}'@'%'"
-mysql -uroot <<< "FLUSH PRIVILEGES"
-echo "Done!"
+
+if ! mysql -uroot -e "use $db" 2>/dev/null; then
+    info "Adding MySQL database: ${db}"
+
+    mysql -uroot <<< "CREATE DATABASE ${db} CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci"
+    mysql -uroot <<< "CREATE USER '${db}'@'%' IDENTIFIED BY ''"
+    mysql -uroot <<< "GRANT ALL PRIVILEGES ON ${db}.* TO '${db}'@'%'"
+    mysql -uroot <<< "FLUSH PRIVILEGES"
+    echo "Done!"
+fi
